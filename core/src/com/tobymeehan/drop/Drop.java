b81ef108cd4ef2be3c3030fcf7d739d509ff9d32
TobyMeehan <toby@tobymeehan.com>
2022-11-30 00:48:19 +0000

    Add camera and spritebatch

--- core/src/com/tobymeehan/drop/Drop.java
+++ core/src/com/tobymeehan/drop/Drop.java
@@ -2,6 +2,7 @@ package com.tobymeehan.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -9,12 +10,20 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Drop extends ApplicationAdapter {
 	private Texture dropImage;
 	private Texture bucketImage;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
 	
 	@Override
 	public void create () {
 
 		dropImage = new Texture(Gdx.files.internal("drop.png"));
 		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 400);
+
+		batch = new SpriteBatch();
 	}
 
 	@Override
