d6cbd2c6a548249fede123a7b539c2a937b255c5
TobyMeehan <toby@tobymeehan.com>
2022-11-30 01:06:13 +0000

    Add raindrop logic
    Added logic to spawn and collect falling raindrops

--- core/src/com/tobymeehan/drop/Drop.java
+++ core/src/com/tobymeehan/drop/Drop.java
@@ -6,9 +6,15 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+import org.w3c.dom.css.Rect;
+
+import java.util.Iterator;
 
 public class Drop extends ApplicationAdapter {
 	private Texture dropImage;
@@ -18,6 +24,8 @@ public class Drop extends ApplicationAdapter {
 	private SpriteBatch batch;
 
 	private Rectangle bucket;
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
 	
 	@Override
 	public void create () {
@@ -35,6 +43,9 @@ public class Drop extends ApplicationAdapter {
 		bucket.y = 20;
 		bucket.width = 64;
 		bucket.height = 64;
+
+		raindrops = new Array<>();
+		spawnRaindrop();
 	}
 
 	@Override
@@ -46,6 +57,9 @@ public class Drop extends ApplicationAdapter {
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		for (Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
 		batch.end();
 
 		if (Gdx.input.isTouched()) {
@@ -60,6 +74,29 @@ public class Drop extends ApplicationAdapter {
 
 		if (bucket.x < 0) bucket.x = 0;
 		if (bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+		if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext();) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+
+			if (raindrop.y + 64 < 0) iter.remove();
+
+			if (raindrop.overlaps(bucket)) {
+				iter.remove();
+			}
+		}
+	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800-64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
 	}
 	
 	@Override
