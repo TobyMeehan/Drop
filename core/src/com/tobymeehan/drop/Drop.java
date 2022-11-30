781b9098540144def72d64a4af3269c052d13b69
TobyMeehan <toby@tobymeehan.com>
2022-11-30 00:46:35 +0000

    Load texture assets
    Loaded images as assets

--- core/src/com/tobymeehan/drop/Drop.java
+++ core/src/com/tobymeehan/drop/Drop.java
@@ -1,31 +1,29 @@
 package com.tobymeehan.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Drop extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private Texture dropImage;
+	private Texture bucketImage;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		dropImage = new Texture(Gdx.files.internal("drop.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
