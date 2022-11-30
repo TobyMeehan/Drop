b8a1168bc05ddbba2f4588fa3f954d42be304b6a
TobyMeehan <toby@tobymeehan.com>
2022-11-30 00:42:48 +0000

    Configure desktop launcher

--- desktop/src/com/tobymeehan/drop/DesktopLauncher.java
+++ desktop/src/com/tobymeehan/drop/DesktopLauncher.java
@@ -10,6 +10,8 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
 		new Lwjgl3Application(new Drop(), config);
 	}
 }
